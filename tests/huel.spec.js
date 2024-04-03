16e2c15f838e1a5fb07ea47f569ba92a30501d60
Tom Webb <tomwebb1992@gmail.com>
2024-04-03 19:55:19 +0100

    accepted cookied button

--- tests/huel.spec.js
+++ tests/huel.spec.js
@@ -2,7 +2,7 @@ const { chromium } = require("playwright");
 const { test, expect } = require("@playwright/test");
 // installing playwright for testing purposes.
 test("select 2 items for basket", async () => {
-  const browser = await chromium.launch({ timeout: 0 });
+  const browser = await chromium.launch({ headless: false });
 
   const page = await browser.newPage();
 
@@ -14,11 +14,24 @@ test("select 2 items for basket", async () => {
   await page.waitForSelector('[data-testid="SearchBar__input"]');
   await page.fill(
     '[data-testid="SearchBar__input"]',
-    "complete protein powder",
-    "Enter"
+    "complete protein powder"
   );
-  await page.click('[data-testid="SearchBar__input"]');
-
   await page.press('[data-testid="SearchBar__input"]', "Enter");
-  // select protein powder
+  //Select powder
+  await page.click('a.button:has-text("Shop Powder")');
+  // add coffee caramel
+  await page.click(
+    'button[aria-label="Coffee Caramel Increase Quantity"]',
+    '[data-testid="QuantitySelectorIncrease"]'
+  );
+  await page.click(
+    'button[aria-label="Salted Caramel Increase Quantity"]',
+    '[data-testid="QuantitySelectorIncrease"]'
+  );
+  // accepted cookies as it prevented the ability to continue later on in the test
+  await page.waitForSelector("#onetrust-accept-btn-handler");
+  await page.click("#onetrust-accept-btn-handler");
+
+  await page.waitForSelector('text="Continue"');
+  await page.click('text="Continue"');
 });
