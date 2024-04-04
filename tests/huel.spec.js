c666e205cdc4c36e4695dbb2ef01eed77bb9d2ad
Tom Webb <tomwebb1992@gmail.com>
2024-04-04 11:22:14 +0100

    onto final task

--- tests/huel.spec.js
+++ tests/huel.spec.js
@@ -24,14 +24,8 @@ test("select 2 items for basket", async () => {
   //Select powder
   await page.click('a.button:has-text("Shop Powder")');
   // add coffee caramel
-  await page.click(
-    'button[aria-label="Coffee Caramel Increase Quantity"]',
-    '[data-testid="QuantitySelectorIncrease"]'
-  );
-  await page.click(
-    'button[aria-label="Salted Caramel Increase Quantity"]',
-    '[data-testid="QuantitySelectorIncrease"]'
-  );
+  await page.click('button[aria-label="Coffee Caramel Increase Quantity"]');
+  await page.click('button[aria-label="Salted Caramel Increase Quantity"]');
 
   //Continue to frequency selection
   await page.waitForSelector('text="Continue"');
@@ -44,10 +38,35 @@ test("select 2 items for basket", async () => {
   await page.waitForSelector('text="Continue"');
   await page.click('text="Continue"');
 
-  // search for greens
-  await page.waitForSelector('[data-testid="IconLink-Search"]');
+  //Continue you to basket due to "Before you go page" stopping the test
+  await page.waitForSelector('button:has-text("Continue To Basket")');
+  await page.click('button:has-text("Continue To Basket")');
+
+  // search for nutrition bar
   await page.click('[data-testid="IconLink-Search"]');
   await page.waitForSelector('[data-testid="SearchBar__input"]');
   await page.fill('[data-testid="SearchBar__input"]', "nutrition bar");
-  await page.click('a.button:has-text("Shop Complete Nutrition Bar")', "Enter");
+  await page.click('[data-testid="SearchBar__submit-button"]');
+
+  // Select nutrition bar
+  await page.waitForSelector(
+    'a.button:has-text("Shop Complete Nutrition Bar")'
+  );
+  await page.click('a.button:has-text("Shop Complete Nutrition Bar")');
+
+  //Select  Chocolate Caramel
+  await page.click('button[aria-label="Chocolate Caramel Increase Quantity"]');
+  //click continue
+  await page.waitForSelector('text="Continue"');
+  await page.click('text="Continue"');
+  // One Time Purchase
+  await page.click('text="One Time Purchase"');
+  //click continue
+  await page.waitForSelector('text="Continue"');
+  await page.click('text="Continue"');
+  //Continue you to basket
+  await page.waitForSelector('button:has-text("Continue To Basket")');
+  await page.click('button:has-text("Continue To Basket")');
+
+  //Verify 2 or more items in basket
 });
